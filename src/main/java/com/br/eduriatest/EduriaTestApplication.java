9976a13e97f1532ef42057a1d5d09780d4d71cea
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2020-08-24 23:14:36 -0300

    First commit

---
+++ src/main/java/com/br/eduriatest/EduriaTestApplication.java
@@ -0,0 +1,13 @@
+package com.br.eduriatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduriaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduriaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/eduriatest/EduriaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.eduriatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduriaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
