edb7f31abaf98984b5b8c4ad1b75143a4da39c2a
Vanderlei Leite Loureiro <desenvolvedor8@f5promotora.com.br>
2020-09-03 08:54:13 -0300

    Answer all the questions method

--- src/main/java/com/br/eduriatest/controller/EvaluatorController.java
+++ src/main/java/com/br/eduriatest/controller/EvaluatorController.java
@@ -1,11 +1,14 @@
 package com.br.eduriatest.controller;
 
+import com.br.eduriatest.model.EvaluatorTestForm;
 import com.br.eduriatest.model.EvaluatorTestResult;
 import com.br.eduriatest.service.EvaluatorService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -20,9 +23,9 @@ public class EvaluatorController {
         this.evaluatorService = evaluatorService;
     }
 
-    @GetMapping(("/{id}"))
-    public ResponseEntity<EvaluatorTestResult> testEvaluator(@PathVariable Long id) {
-        return ResponseEntity.ok().body(this.evaluatorService.testEvaluator(id));
+    @PostMapping
+    public ResponseEntity<EvaluatorTestResult> testEvaluator(@RequestBody EvaluatorTestForm form) {
+        return ResponseEntity.ok().body(this.evaluatorService.testEvaluator(form));
     }
     
 }
--- src/main/java/com/br/eduriatest/service/EvaluatorService.java
+++ src/main/java/com/br/eduriatest/service/EvaluatorService.java
@@ -1,21 +1,59 @@
 package com.br.eduriatest.service;
 
 import com.br.eduriatest.client.EvaluatorClient;
+import com.br.eduriatest.model.AnswerQuestionForm;
+import com.br.eduriatest.model.EvaluatorTestForm;
 import com.br.eduriatest.model.EvaluatorTestResult;
+import com.br.eduriatest.model.QuestionPresentedDto;
+import com.br.eduriatest.model.StudentLevel;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EvaluatorService {
 
+	private int CORRECT_ALTERNATIVE = 3;
+	private int WRONG_ALTERNATIVE   = 1;
+	
     private EvaluatorClient evaluatorClient;
 
     EvaluatorService(EvaluatorClient evaluatorClient) {
         this.evaluatorClient = evaluatorClient;
     }
 
-    public EvaluatorTestResult testEvaluator(Long id) {
+    public EvaluatorTestResult testEvaluator(EvaluatorTestForm form) {
+    	
+    	this.answerAllQuestions(form);
+    	this.getFinalResult();
         return null;
     }
+    
+    private void getFinalResult() {
+    	// todo
+    }
+    
+    private void answerAllQuestions(EvaluatorTestForm form) {
+    	for(int i = 0; i < 50; i++) {
+    		ResponseEntity<QuestionPresentedDto> presentatedQuestion = this.evaluatorClient.getQuestion(form.getIdEnrollment());
+    		AnswerQuestionForm answerForm = new AnswerQuestionForm();
+    		answerForm.setEnrollmentId(form.getIdEnrollment());
+    		answerForm.setQuestionId(presentatedQuestion.getBody().getQuestionId());
+    		answerForm.setSelectedAlternative(this.answerByStudentLevel(form.getStudentLevel(), i));
+    	}
+    }
+    
+    private int answerByStudentLevel(StudentLevel level, int indexFor) {
+    	if (level == StudentLevel.ADVANCED) 
+    		return this.CORRECT_ALTERNATIVE;	
+    	
+    	if (level == StudentLevel.BEGINNER) 
+    		return (indexFor % 5 == 0) ? this.CORRECT_ALTERNATIVE : this.WRONG_ALTERNATIVE;
+    	
+    	return (indexFor % 2 == 0) ? this.CORRECT_ALTERNATIVE : this.WRONG_ALTERNATIVE;
+    }
+    
+    
+    
     
 }
